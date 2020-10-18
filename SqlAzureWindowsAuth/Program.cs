fde7947f5e61f8aef98718f8b92b502c6c7260e7
Farooq Mahmud <f>
2020-10-18 12:36:03 -0700

    wip

---
+++ SqlAzureWindowsAuth/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlAzureWindowsAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=fm-d-usw2-sql-demo.database.windows.net;Authentication=Active Directory Integrated; Initial Catalog=demo;";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            Console.WriteLine("Connected!");
+        }
+    }
+}
+
+
